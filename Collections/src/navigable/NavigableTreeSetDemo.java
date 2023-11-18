15e34d2b38f3cb439c4df59d85a2c544a5420aa9
naivedeveloper95 <findsatyam95@gmail.com>
2023-11-18 15:14:36 +0530

    Added changes

--- Collections/src/navigable/NavigableTreeSetDemo.java
+++ Collections/src/navigable/NavigableTreeSetDemo.java
@@ -6,25 +6,21 @@ public class NavigableTreeSetDemo {
 
 	public static void main(String[] args) {
 
-		TreeSet<Integer> s = new TreeSet<Integer>();
+		TreeSet<Integer> treeSet = new TreeSet<Integer>();
 
-		s.add(10);
-		s.add(20);
-		s.add(30);
-		s.add(40);
-		s.add(50);
+		treeSet.add(10);
+		treeSet.add(20);
+		treeSet.add(30);
+		treeSet.add(40);
+		treeSet.add(50);
 
-		System.out.println(s);
-
-		System.out.println(s.ceiling(20));
-		System.out.println(s.higher(40));
-		System.out.println(s.floor(10));
-		System.out.println(s.lower(30));
-		System.out.println(s.pollFirst());
-		System.out.println(s.pollLast());
-		System.out.println(s.descendingSet());
-		System.out.println(s);
-
+		System.out.println(treeSet.ceiling(20));
+		System.out.println(treeSet.higher(40));
+		System.out.println(treeSet.floor(10));
+		System.out.println(treeSet.lower(30));
+		System.out.println(treeSet.pollFirst());
+		System.out.println(treeSet.pollLast());
+		System.out.println(treeSet.descendingSet());
 	}
 
 }
