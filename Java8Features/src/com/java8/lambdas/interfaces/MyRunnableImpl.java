009bf44fdc0339e9cc6a91771f5cbe4380d203c1
naivedeveloper95 <findsatyam95@gmail.com>
2023-11-21 16:15:53 +0530

    Added changes

--- Java8Features/src/com/java8/lambdas/interfaces/MyRunnableImpl.java
+++ Java8Features/src/com/java8/lambdas/interfaces/MyRunnableImpl.java
@@ -4,7 +4,7 @@ public class MyRunnableImpl implements Runnable {
 
 	@Override
 	public void run() {
-		for(int i=1;i<=10;i++){
+		for (int i = 1; i <= 10; i++) {
 			System.out.println("Child Thread");
 		}
 
--- Java8Features/src/com/java8/lambdas/interfaces/Test.java
+++ Java8Features/src/com/java8/lambdas/interfaces/Test.java
@@ -3,16 +3,14 @@ package com.java8.lambdas.interfaces;
 public class Test {
 
 	public static void main(String[] args) {
-		// Runnable r = new MyRunnableImpl();
-		// Thread t = new Thread(r);
-		// t.start();
+//		Runnable r = new MyRunnableImpl();
+//		Thread t = new Thread(r);
+//		t.start();
 
 		Runnable r = () -> {
-
 			for (int i = 1; i <= 10; i++) {
 				System.out.println("Child Thread");
 			}
-
 		};
 
 		Thread t = new Thread(r);
