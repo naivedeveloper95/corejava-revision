2d7e8ca53f4d7bba262e15ae3f9bb93db2c98616
naivedeveloper95 <findsatyam95@gmail.com>
2023-11-22 12:42:43 +0530

    Added changes

--- Java8Features/src/com/java8/optional/OptionalDemo.java
+++ Java8Features/src/com/java8/optional/OptionalDemo.java
@@ -9,6 +9,19 @@ public class OptionalDemo {
 		System.out.println("isPresent " + empty.isPresent());
 		Optional<String> data = Optional.of("Optional but very useful");
 		System.out.println("isPresent " + data.isPresent());
+		System.out.println("Value: " + data.get());
+
+		if (empty.isPresent())
+			System.out.println(empty.get());
+
+		System.out.println(data.filter((s) -> s.equals("Optional but very useful")));
+		System.out.println(data.filter((s) -> s.equals("Optional very useful")));
+
+		System.out.println(data.orElse("Default Value"));
+		System.out.println(empty.orElse("Default Value"));
+
+		data.ifPresent(System.out::println);
+		empty.ifPresent(System.out::println);
 	}
 
 }
